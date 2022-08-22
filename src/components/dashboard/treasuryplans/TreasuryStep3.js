986a9dab9866ef7e3e9e27bba7927294fd2a1f88
Precious-Macaulay <preciousmacaulay001@gmail.com>
2022-08-22 13:02:19 +0100

    fund wallet bugs fix

--- src/components/dashboard/treasuryplans/TreasuryStep3.js
+++ src/components/dashboard/treasuryplans/TreasuryStep3.js
@@ -32,7 +32,7 @@ const TreasuryStep3 = () => {
     console.log(data);
   };
 
-  const childData = async () => {
+  const fetchChildData = async () => {
     const mainLink = "https://earli.herokuapp.com";
     const mainLink1 = "http://localhost:2004";
 
@@ -108,7 +108,7 @@ const TreasuryStep3 = () => {
 
   useEffect(() => {
     fetchData();
-    childData();
+    fetchChildData();
   }, []);
 
   return (
